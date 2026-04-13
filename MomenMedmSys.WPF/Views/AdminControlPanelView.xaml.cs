55d27f584f39d09a0b966de9dc4339e1b5718b7d
Momen <mom2n@example.com>
2026-04-13 23:31:33 +0200

    feat(ui): standardize card sizes, fix AdminCP layout, add print export

--- MomenMedmSys.WPF/Views/AdminControlPanelView.xaml.cs
+++ MomenMedmSys.WPF/Views/AdminControlPanelView.xaml.cs
@@ -1,8 +1,18 @@
+using System.Windows;
 using System.Windows.Controls;
+
 namespace MomenMedmSys.WPF.Views
 {
     public partial class AdminControlPanelView : UserControl
     {
         public AdminControlPanelView() => InitializeComponent();
+
+        private void TxtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ViewModels.AdminControlPanelViewModel vm && sender is PasswordBox pb)
+            {
+                vm.Password = pb.Password;
+            }
+        }
     }
 }
